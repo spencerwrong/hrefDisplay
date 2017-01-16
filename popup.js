7a15b28c3b99454ca620aef2a1795bae2246862c
Spencer Wong <spencerwong@100pp-1103.local>
2017-01-16 15:47:21 -0800

    changed console.log to document.write

--- popup.js
+++ popup.js
@@ -1,58 +1,38 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var link = document.getElementById('link');
-    function getAllLinks()
-    {
-      var arr = []
-      var l = document.links;
-      for(var i=0; i<l.length; i++)
-      {
-        arr.push(l[i].href);
+function getAllLinks() {
+  var arr = []
+  var l = document.links;
+  for(var i=0; i<l.length; i++) {
+    arr.push(l[i].href);
+  }
+  return arr;
+}
+
+function countLinks(arr) {
+  var links = new Array();
+  var counts = new Array();
+  if(arr.length > 0) {
+    for(var i = 0; i < arr.length; i++) {
+      var currentLink = arr[i];
+      if(!links.includes(currentLink)) {
+        links.push(currentLink);
+        counts.push(1);
       }
-      return arr;
-    }
-
-    function countLinks(arr)
-    {
-      var links = new Array();
-      var counts = new Array();
-
-      if(arr.length > 0)
-      {
-        for(var i = 0; i < arr.length; i++)
-        {
-          var currentLink = arr[i];
-          if(!links.includes(currentLink))
-          {
-            links.push(currentLink);
-            counts.push(1);
-          }
-          else
-          {
-            for(var j = 0; j < counts.length; j++)
-            {
-              if(currentLink == links[j])
-              {
-                counts[j]++;
-              }
-            }
+      else {
+        for(var j = 0; j < counts.length; j++) {
+          if(currentLink == links[j]) {
+            counts[j]++;
           }
         }
       }
-      arr = [links, counts];
-      return arr;
     }
+  }
+  arr = [links, counts];
+  return arr;
+}
 
-    function run()
-    {
-      var finalArr = countLinks(getAllLinks()); //2x2 array
-      for(var i = 0; i < finalArr[0].length; i++)
-      {
-        console.log(finalArr[0][i] + ": " + finalArr[1][i]);
-      }
-    }
-    link.addEventListener('click', function() {
-        run();
-    });
-});
-
-
+function printLinks() {
+  var finalArr = countLinks(getAllLinks()); //2x2 array
+  for(var i = 0; i < finalArr[0].length; i++) {
+    document.write(finalArr[0][i] + ": " + finalArr[1][i] + "<br>");
+  }
+}
